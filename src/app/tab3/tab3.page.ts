6f53862ad09618bb76083cdaa88c20f09ac7ee0c
Tolga Baris Pinar <68820772+tolgabp@users.noreply.github.com>
2023-05-26 12:33:06 +0100

    first commit after class

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor(public photoService: PhotoService) {}
+
+}
